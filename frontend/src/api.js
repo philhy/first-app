ccaf4d2bdb72328ba78b9dd3ad2c50066d4d2038
philhy <153537098+philhy@users.noreply.github.com>
2024-12-09 21:17:33 -0600

    api.js update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -7,6 +7,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
+console.log(api)
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
