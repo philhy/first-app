bd2145c99ddc7ac4d0eeac73724ea06fb9da7c51
philhy <153537098+philhy@users.noreply.github.com>
2024-12-11 07:49:03 -0600

    nfl stats add

--- backend/api/views.py
+++ backend/api/views.py
@@ -56,6 +56,7 @@ def user_info(request):
             "username": user.username,
             "first_name": user.first_name,
             "last_name": user.last_name,
+            "email": user.email,
         })
     else:
         return Response({
